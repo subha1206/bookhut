7cf3f8f113cbc6a65c958c57b69aeea010d013c3
Subha Sarkar <sarkarsubha1206@gmail.com>
2020-12-09 22:23:32 +0530

    :zap: added react-lazy for code spliting, :sparkles: react-tostify for notifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,46 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './containers/home';
-import Login from './containers/login';
-import Register from './containers/register';
-import ProductDetails from './containers/productDetails';
-import Shipping from './containers/shipping';
-import Cart from './containers/cart';
-import Payment from './containers/payment';
-import PlaceOrder from './containers/placeOrder';
-import Order from './containers/order';
-import Profile from './containers/profile';
-
+import Loader from './components/common/loader';
 import './App.css';
-
 import Header from './components/common/header';
+import Order from './containers/order';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Login = React.lazy(() => import('./containers/login'));
+const Register = React.lazy(() => import('./containers/register'));
+const ProductDetails = React.lazy(() => import('./containers/productDetails'));
+const Shipping = React.lazy(() => import('./containers/shipping'));
+const Cart = React.lazy(() => import('./containers/cart'));
+const PlaceOrder = React.lazy(() => import('./containers/placeOrder'));
+const Profile = React.lazy(() => import('./containers/profile'));
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/products/:id" component={ProductDetails} />
-        <Route path="/cart/:id?" component={Cart} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/shipping" component={Shipping} />
-        <Route path="/payment" component={Payment} />
-        <Route path="/order" component={PlaceOrder} exact />
-        <Route path="/order/:id" component={Order} />
-      </Switch>
-    </Router>
+    <Suspense
+      fallback={
+        <div style={{ textAlign: 'center', marginTop: '10em' }}>
+          <Loader />
+        </div>
+      }
+    >
+      <Router>
+        <Header />
+        <ToastContainer className="toast-container" />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/products/:id" component={ProductDetails} />
+          <Route path="/cart/:id?" component={Cart} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/shipping" component={Shipping} />
+          <Route path="/order" component={PlaceOrder} exact />
+          <Route path="/order/:id" component={Order} />
+        </Switch>
+      </Router>
+    </Suspense>
   );
 }
 
--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -2,13 +2,18 @@ import axios from 'axios';
 import userConstants from '../constants/userConstants';
 
 import { apiEndPoints } from '../../helper/API';
+import notify from '../../helper/notify';
 export const login = (loginCred) => async (dispatch) => {
   try {
     dispatch({ type: userConstants.USER_LOGIN_REQUEST });
 
     const { data } = await axios.post(apiEndPoints.USER_LOGIN, loginCred);
+    if (data.status === 'success') {
+      notify(data.status, 'Logged in successfully');
+    }
     dispatch({ type: userConstants.USER_LOGIN_SUCCESS, payload: data });
   } catch (err) {
+    notify(err.response.data.status, err.response.data.message);
     dispatch({ type: userConstants.USER_LOGIN_FAIL });
   }
 };
@@ -22,11 +27,15 @@ export const register = (registerData, history) => async (dispatch) => {
   try {
     dispatch({ type: userConstants.USER_REGISTER_REQUEST });
     const { data } = await axios.post(apiEndPoints.USER_REGISTER, registerData);
+    if (data.status === 'success') {
+      notify(data.status, 'Account created successfully');
+    }
     dispatch({ type: userConstants.USER_REGISTER_SUCCESS });
     dispatch({ type: userConstants.USER_LOGIN_SUCCESS, payload: data });
 
     history.push('/');
   } catch (err) {
+    notify(err.response.data.status, err.response.data.message);
     dispatch({ type: userConstants.USER_REGISTER_FAIL });
   }
 };
@@ -41,10 +50,16 @@ export const uploadProfilePic = (formData) => async (dispatch, getState) => {
     if (token) {
       axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
     }
+
     const { data } = await axios.post(
       apiEndPoints.USER_UPLOAD_PROFILE_PICTURE,
       formData
     );
+    if (data.status === 'success') {
+      notify(data.status, 'Profile picture uploaded successfully');
+    }
     dispatch({ type: userConstants.USER_UPLOAD_PROFILE_PIC, payload: data });
-  } catch (err) {}
+  } catch (err) {
+    notify(err.response.data.status, err.response.data.message);
+  }
 };
